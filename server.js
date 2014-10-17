e5de0b45df7f6dbc82adfd3633d1f90701f872a0
John Lau <johnlau@Johns-MacBook-Air.local>
2014-10-17 13:21:14 +0800

    Initial Commit

--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 var express = require('express');
 var stringify = require('csv-stringify');
+
 var app = express();
+app.use(express.bodyParser());
 
-var data = [["id", "subject1", "subject2", "subject3"], ["jack", 85, 90, 68], ["sam", 77, 89, 69]];
+//var filename = "test";
+//var data = [["id", "subject1", "subject2", "subject3"], ["jack", 85, 90, 68], ["sam", 77, 89, 69]];
+// To test e.g. http://localhost:3000/download/csv?filename=test&data=[["id", "subject1", "subject2", "subject3"], ["jack", 85, 90, 68], ["sam", 77, 89, 69]]
 
-app.get('/download', function(req, res){
+// Note: to call this service from front-end layer, use:-
+// window.open('/download/csv?filename=' + filename + '&data=' + encodeURIComponent(JSON.stringify(data)));
+
+app.get('/download/csv', function(req, res){
+    
+    //console.log(req.query);
+    var filename = req.query.filename;
+    var data = req.query.data;
+
+    if(typeof(data) != 'object' && data != undefined) {
+        data = JSON.parse(data);
+    } 
 
     stringify(data, function(err, output){
-        res.setHeader('Content-disposition', 'attachment; filename=testing.csv');
-        res.set('Content-Type', 'text/csv');
-        //console.log(res);
-        res.send(output);
+        if(err) {
+            res.send(404, err);
+        }
+        else {
+            res.setHeader('Content-disposition', 'attachment; filename=' + filename + '.csv');
+            res.set('Content-Type', 'text/csv');
+            res.send(output);
+        }
     });
     
 });
